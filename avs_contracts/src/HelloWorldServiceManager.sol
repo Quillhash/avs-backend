fef497e0dcb27c87e8d2203e1e0e3b7f8bafd4bd
Chitranshu <varshneychinu1204@gmail.com>
2024-10-30 15:39:52 +0530

    update

--- avs_contracts/src/HelloWorldServiceManager.sol
+++ avs_contracts/src/HelloWorldServiceManager.sol
@@ -127,6 +127,7 @@ contract HelloWorldServiceManager is
     function createNewAuditTask(
         address contractAddress
     ) external returns (Task memory) {
+        latestTaskNum += 1;
         submitContract(contractAddress, false);
         // create a new task struct
         Task memory auditTask;
@@ -139,7 +140,6 @@ contract HelloWorldServiceManager is
 
         // emit audit task event
         emit AuditTaskCreated(latestTaskNum, auditTask);
-        latestTaskNum += 1;
 
         return auditTask;
     }
@@ -248,9 +248,9 @@ contract HelloWorldServiceManager is
 
         // store hash of task onchain, emit event, and increase taskNum
         // allTaskHashes[latestTaskNum] = keccak256(abi.encode(insuranceTask));
-        allTaskHashes[uint32(_submissionId)] = keccak256(
-            abi.encode(insuranceTask)
-        );
+        // allTaskHashes[uint32(_submissionId)] = keccak256(
+        //     abi.encode(insuranceTask)
+        // );
         // emit InsuranceTaskCreated(latestTaskNum, insuranceTask);
         emit InsuranceTaskCreated(uint32(_submissionId), insuranceTask);
         // latestTaskNum += 1;
@@ -266,12 +266,8 @@ contract HelloWorldServiceManager is
     ) external onlyOperator {
         // check that the task is valid, hasn't been responsed yet, and is being responded in time
         require(
-            keccak256(abi.encode(task)) == allTaskHashes[referenceTaskIndex],
-            "supplied task does not match the one recorded in the contract"
-        );
-        require(
-            !insuranceTaskCheck[referenceTaskIndex],
-            "Operator has already responded to the task"
+            policies[referenceTaskIndex].owner != address(0),
+            "policy has not created"
         );
 
         // // The message that was signed
@@ -312,6 +308,10 @@ contract HelloWorldServiceManager is
         uint256 _policyId,
         string memory _evidenceIPFSHash
     ) public {
+        require(
+            insuranceTaskCheck[uint32(_policyId)],
+            "operator not approved insurance"
+        );
         Policy storage policy = policies[_policyId];
         // require(
         //     msg.sender == policy.owner,
