272e8988a6c2e5b3a6f66b1051094638fe9f26b9
Akshat Mishra <akshatmishra@Akshats-MacBook-Pro.local>
2024-11-02 01:58:15 +0530

    Node Operator Client Code Cleanup

--- avs_contracts/script/deployContract.s.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.26;
-
-import {Script, console} from "forge-std/Script.sol";
-
-contract deployScript is Script {
-    // function setUp() public {}
-    // function run() public {}
-}
